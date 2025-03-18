6180a2f7e4f88d5782657012a35f42cec5e16c2d
Cacadude <129430626+Cacadude@users.noreply.github.com>
2025-03-18 22:44:38 +0300

    Update Bot_psy.py

--- Bot_psy.py
+++ Bot_psy.py
@@ -37,7 +37,7 @@ def handle(data):
         # Генерация случайного User-Agent
         try:
             ua = UserAgent()
-            user_agent = ua.chrome  # Или ua.random для полностью случайного
+            user_agent = ua.random  
         except:
             user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
         
